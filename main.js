de57ce74613932ccfe59f48d07e51cf9d0e4f74a
Milos Rajkovic <milosrajkovic98@yahoo.com>
2020-09-11 21:16:28 +0200

    promena lobby dizajna

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const socket = io("http://localhost:3000");
 socket.on("welcome", (data) => {
   console.log(data);
   console.log("id korisnika je", socket.id);
+  document.getElementById("player-id-paragraph").innerHTML =
+    "Socket ID: " + socket.id;
 });
 
 const createRoomBtn = document.querySelector("#create-room-btn");
@@ -19,28 +21,26 @@ joinRoomBtn.addEventListener("click", () => {
   socket.emit("room:join", roomCodeInput.value);
 });
 
-
 //povratne informacije o kreiranoj sobi
 socket.on("room:created", (data) => {
   //   const roomCode = data.roomCode;
   console.log("You have joined room", data);
 
-  document.getElementById('lobby').style.display = 'none';
+  document.getElementById("lobby").style.display = "none";
 });
 
 //povratne informacije o uspesnoj konekciji u sobu
 socket.on("room:joined", (data) => {
-
   console.log("You have joined room", data);
-  document.getElementById('lobby').style.display = 'none';
+  document.getElementById("lobby").style.display = "none";
 });
 
 //informacije o novom korisniku u sobi u kojoj je trenutni korisnik
 socket.on("room:userJoined", (userID) => {
   console.log("User", userID, "has joined the room.");
-})
+});
 
+//notifikacija izlaska iz sobe
 socket.on("room:userLeft", (userID) => {
-
   console.log("User", userID, "has left the room.");
-})
+});
