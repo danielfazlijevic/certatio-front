93b9b798372a3d9f3bbc9f6d5e592a32d3198d61
Milos Rajkovic <milosrajkovic98@yahoo.com>
2020-09-11 13:47:57 +0200

    Komentarisane i sredjene konekcije

--- main.js
+++ main.js
@@ -6,19 +6,27 @@ socket.on("welcome", (data) => {
 });
 
 const createRoomBtn = document.querySelector("#create-room-btn");
+const joinRoomBtn = document.querySelector("#join-room-btn");
+const roomCodeInput = document.querySelector("#room-code");
 
+//emitovanje zahteva za create
 createRoomBtn.addEventListener("click", () => {
   socket.emit("room:create", "MilesGayne");
 });
 
+//emitovanje zahteva za join
+joinRoomBtn.addEventListener("click", () => {
+  socket.emit("room:join", roomCodeInput.value);
+});
+
+
+//povratne informacije o kreiranoj sobi
 socket.on("room:created", (data) => {
   //   const roomCode = data.roomCode;
-  console.log(data);
-
-  socket.join(data);
-  //   alert(`Room created, your code is ${roomCode}`);
+  console.log("You have joined room", data);
 });
 
-socket.emit("join room", {
-  code: "abc"
-});
+//informacije o novom korisniku u sobi u kojoj je trenutni korisnik
+socket.on("room:userJoined", (userID) => {
+  console.log("User", userID, "has joined the room.");
+})
